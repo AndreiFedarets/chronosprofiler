6e93aeed61ba6c31f0f08487bb1003614e8bf16f
Andrei Fedarets <andrei.fedarets@gmail.com>
2019-12-26 22:25:33 +0300

    Migrating to VS2017 Removing binaries (will be replaced with nuget later)

--- source/Chronos/Chronos.Agent/Formatter.cpp
+++ source/Chronos/Chronos.Agent/Formatter.cpp
@@ -10,9 +10,9 @@ namespace Chronos
 			const size_t bufferLength = 1024;
 			__wchar buffer[bufferLength] = {0};
 			va_list arglist;
-			_crt_va_start(arglist, format);
+			va_start(arglist, format);
 			int returnCount = _vswprintf_c_l(buffer, bufferLength, format.c_str(), NULL, arglist);
-			_crt_va_end(arglist);
+			va_end(arglist);
 			__string result(buffer);
 			return result;
 		}
